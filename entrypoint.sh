652e2c3fdf9fe5683c591a9a9a5f961b06a280ff
Nicolas Vanheuverzwijn <nicolas.vanheu@gmail.com>
2017-03-31 13:26:56 -0400

    feat: send SIGTERM to remaining process after the stop.d scripts are called

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,30 @@ process_scripts() {
 	local path=$1
 
 	for script in $(ls $path | sort -n); do
-		echo "> ${path}/${script}... processing"
+		echo "> Sourcing '${path}/${script}'.."
 		source "${path}/${script}"
 	done
 }
 
+kill_remaining_process() {
+	# Gather all PIDs except for pid 1 (entrypoint script) into a
+	# space separated list, send SIGTERM and wait for those process to finish properly
+	PIDS=`ps -e | grep -v --exclude=' ${BASHPID} ' -E "PID|\s1\s" | awk 'BEGIN { ORS=" " }; {print $1}'`
+	kill -TERM $PIDS >> /tmp/pidtokill
+
+	for PID in $PIDS; do
+		while [[ -d /proc/$PID ]]; do
+			sleep 0.1
+		done
+	done
+}
+
 finish() {
+	trap "" SIGTERM SIGQUIT SIGINT
 	echo '> Stopping all services..'
 	process_scripts "${ENTRYPOINT_ROOT}/stop.d"
+	echo '> Killing remaining process..'
+	kill_remaining_process
 	echo '> Shutting down now.'
 }
 
